3c16e6d962796644aa4e6c6ce6261fd095e4e2ae
Tobias Gruber <22395758+TobiasGruberWeb@users.noreply.github.com>
2020-06-10 11:28:31 +0200

    updated groovy to clean up sites tree
    refactored to use targeted node types for filtering

--- targeted/Remove default Error Pages.groovy
+++ targeted/Remove default Error Pages.groovy
@@ -2,7 +2,6 @@ import org.parosproxy.paros.network.HttpMessage
 import org.parosproxy.paros.model.Model
 import org.parosproxy.paros.model.SiteMap
 import org.parosproxy.paros.model.SiteNode
-import org.parosproxy.paros.extension.history.ExtensionHistory
 import org.apache.commons.lang3.ArrayUtils
 import java.lang.*
 import org.parosproxy.paros.network.HttpSender
@@ -13,101 +12,108 @@ import groovy.transform.Field
 import net.htmlparser.jericho.*
 import org.apache.commons.codec.digest.DigestUtils
 import java.util.function.Predicate
+import org.parosproxy.paros.network.HttpHeader
+import org.parosproxy.paros.core.scanner.Alert
+import org.zaproxy.zap.extension.history.PopupMenuPurgeSites
 
+@Field final int alertThreshold = Alert.RISK_MEDIUM
 @Field final HttpSender sender = new HttpSender(Model.getSingleton().getOptionsParam().getConnectionParam(), true, 6)
 
 void invokeWith(HttpMessage msg){
-	HttpMessage errorPage1 = getErrorPage(msg)
-	HttpMessage startPage = getStartPage(msg)
-	final int statusCode = errorPage1.getResponseHeader().getStatusCode()
-	println(statusCode)
-	if (statusCode == 404) {
-		println("Filter based on status code 404")
-		Predicate<SiteNode> statusCodeFilter = { node -> node.getHistoryReference() != null && node.getHistoryReference().getStatusCode() == statusCode }
-		filterTree(msg, statusCodeFilter)
-	}
-	else if (statusCode >= 301 && statusCode <= 302) {
-		String redirectLocation = errorPage1.getRequestHeader().getHeader("Location")
-		// TODO handle redirect location
-	}
-	else if (statusCode == 200) {
-		HttpMessage errorPage2 = getErrorPage(msg)
-		String errorPageTitle2 = getTitle(errorPage2)
-		String errorPageTitle1 = getTitle(errorPage1)
-		String startPageTitle = getTitle(startPage)
-		if (errorPageTitle1.equals(errorPageTitle2) && !startPageTitle.equals(errorPageTitle1)) {
-			println("Error Page Title 1 " + errorPageTitle1)
-			println("Error Page Title 2 " + errorPageTitle2)
-			println("Start Page Title " + startPageTitle)
-			println("Filter by title [" + errorPageTitle1 + "]")
-			Predicate<SiteNode> titleFilter = { node -> 
-												if (node.getHistoryReference() != null) {
-													HttpMessage nodeMsg = node.getHistoryReference().getHttpMessage()
-													return StringUtils.equals(getTitle(nodeMsg), errorPageTitle1)
-												}
-										return false
-										}
-			filterTree(msg, titleFilter)
-		}
-		else {
-			String errorPageHash1 = DigestUtils.sha256Hex(errorPage1.getResponseBody().toString())
-			String errorPageHash2 = DigestUtils.sha256Hex(errorPage2.getResponseBody().toString())
-			if (errorPageHash1.equals(errorPageHash2)) {
-				println("Error Page 1 Hash: " + errorPageHash1)
-				println("Error Page 2 Hash: " + errorPageHash2)	
-				println("Calculate Hashes on full error page")
-				Predicate<SiteNode> hashFilter = { node -> 
-												String responseBodyAsString = getBodyFromNode(node)
-												if (responseBodyAsString != null) {
-													return StringUtils.equals(DigestUtils.sha256Hex(responseBodyAsString).toString(), errorPageHash1)
-												}
-												return false
-											}
-				filterTree(msg, hashFilter)
-			}
-			else
-			{
-				println("Calculate hash depending on most common part")
-				String[] splittedErrorPage1 = errorPage1.getResponseBody().toString().split("\n")
-				String[] splittedErrorPage2 = errorPage2.getResponseBody().toString().split("\n")
-				List<String> startEquals = new ArrayList<String>()
-				List<String> endEquals = new ArrayList<String>()
-				int length = Math.min(splittedErrorPage1.length, splittedErrorPage2.length)
-				for (int i = 0; i < length; i++) {
-					if (StringUtils.equals(splittedErrorPage1[i], splittedErrorPage2[i])) {
-						startEquals.add(splittedErrorPage1[i])
-					}
-					else {
-						break
-					}
-				}
-				for (int j = 0; j < length; j++) {
-					if (StringUtils.equals(splittedErrorPage1[splittedErrorPage1.length-(j + 1)], splittedErrorPage2[splittedErrorPage2.length-(j + 1)])) {
-						endEquals.add(splittedErrorPage1[splittedErrorPage1.length-(j + 1)])
-					}
-					else {
-						break
-					}
-				}
-				String startHash = DigestUtils.sha256Hex(StringUtils.join(startEquals, "\n"))
-				Collections.reverse(endEquals)
-				String endHash = DigestUtils.sha256Hex(StringUtils.join(endEquals, "\n"))
-				println("Start Equals: " + startEquals.size())
-				println("End Equals: " + endEquals.size())
-				println("Start Hash " + startHash)
-				println("End Hash " + endHash)
+	if (isValidStartPoint(msg)) {
+		int originalMessageStatusCode = msg.getResponseHeader().getStatusCode()
+		String originalResponseBody = msg.getResponseBody().toString()
+
+		HttpMessage testMessage = msg.cloneRequest()
+		sender.sendAndReceive(testMessage, false)
+
+		int testMessageStatusCode = testMessage.getResponseHeader().getStatusCode()
+		String testResponseBody = testMessage.getResponseBody().toString()
+
+		List<Predicate<SiteNode>> predicates = new ArrayList()
+
+		if (originalMessageStatusCode == testMessageStatusCode) {
+			println()
+			if (testMessage.getResponseHeader().isText()) {
+				// Standard Predicates
+				Predicate<SiteNode> nodeNotNullFilter = { node -> node != null }
+				Predicate<SiteNode> historyReferenceFilter = { node -> node.getHistoryReference() != null }
+				Predicate<SiteNode> statusCodeFilter = { node ->  node.getHistoryReference().getStatusCode() == testMessageStatusCode }
+				Predicate<SiteNode> requestMethodFilter = { node -> StringUtils.equals(node.getHistoryReference().getMethod(), testMessage.getRequestHeader().getMethod())}
+				Predicate<SiteNode> contentTypeFilter = { node -> StringUtils.equals(node.getHistoryReference().getHttpMessage().getResponseHeader().getHeader(HttpHeader.CONTENT_TYPE), testMessage.getResponseHeader().getHeader(HttpHeader.CONTENT_TYPE))}
+				Predicate<SiteNode> alertFilter = { node -> !node.getHistoryReference().getAlerts().stream().filter({ alert -> alert.getRisk() >= alertThreshold }).findFirst().isPresent()}
 				
-				if (startEquals.size() >= endEquals.size())
-				{
-					Predicate<SiteNode> startPredicate = { node -> return StringUtils.equals(startHash, getStartHashFromBody(node,startEquals.size())) }
-					filterTree(msg, startPredicate)
+				predicates.add(nodeNotNullFilter)
+				predicates.add(historyReferenceFilter)
+				predicates.add(statusCodeFilter)
+				predicates.add(requestMethodFilter)				
+				predicates.add(contentTypeFilter)
+				predicates.add(alertFilter)
+				
+				// Get hash from complete response body
+				String originalMessageHash = DigestUtils.sha256Hex(msg.getResponseBody().toString())
+				String testMessageHash = DigestUtils.sha256Hex(testMessage.getResponseBody().toString())
+				if (StringUtils.equals(originalMessageHash, testMessageHash)) {
+					// Create predicate to filter complete body hashes
+					Predicate<SiteNode> hashFilter = 	{ node -> 	String responseBodyAsString = getBodyFromNode(node)
+															if (responseBodyAsString != null) {
+																return StringUtils.equals(DigestUtils.sha256Hex(responseBodyAsString).toString(), originalMessageHash)
+															}
+															return false
+												}
+					predicates.add(hashFilter)
+				} else {
+					// Create predicate from most commons parts
+					String[] splittedOriginalPage = msg.getResponseBody().toString().split("\n")
+					String[] splittedTestPage = testMessage.getResponseBody().toString().split("\n")
+					List<String> startEquals = new ArrayList<String>()
+					List<String> endEquals = new ArrayList<String>()
+					int length = Math.min(splittedOriginalPage.length, splittedTestPage.length)
+					for (int i = 0; i < length; i++) {
+						if (StringUtils.equals(splittedOriginalPage[i], splittedTestPage[i])) {
+							startEquals.add(splittedOriginalPage[i])
+						} else {
+							break
+						}
+					}
+					for (int j = 0; j < length; j++) {
+						if (StringUtils.equals(splittedOriginalPage[splittedOriginalPage.length-(j + 1)], splittedTestPage[splittedTestPage.length-(j + 1)])) {
+							endEquals.add(splittedOriginalPage[splittedOriginalPage.length-(j + 1)])
+						} else {
+							break
+						}
+					}
+					String startHash = DigestUtils.sha256Hex(StringUtils.join(startEquals, "\n"))
+					Collections.reverse(endEquals)
+					String endHash = DigestUtils.sha256Hex(StringUtils.join(endEquals, "\n"))
+					println("Start Equals: " + startEquals.size())
+					println("End Equals: " + endEquals.size())
+					println("Start Hash " + startHash)
+					println("End Hash " + endHash)
+				
+					if (startEquals.size() >= endEquals.size()) {
+						Predicate<SiteNode> startPredicate = { node -> return StringUtils.equals(startHash, getStartHashFromBody(node,startEquals.size())) }
+						predicates.add(startPredicate)
+					} else {
+						Predicate<SiteNode> endPredicate = { node -> return StringUtils.equals(endHash, getEndHashFromBody(node,endEquals.size())) }
+						predicates.add(endPredicate)
+					}
 				}
-				else {
-					Predicate<SiteNode> endPredicate = { node -> return StringUtils.equals(endHash, getEndHashFromBody(node,endEquals.size())) }
-					filterTree(msg, endPredicate)
-				}
+				
 			}
+			else if (testMessage.getResponseHeader().isEmpty()) {
+				String redirectLocation = errorPage1.getRequestHeader().getHeader("Location")
+			}
+		} else {
+			println("Status Code mismatch original[" + originalMessageStatusCode + "] tested [" + testMessageStatusCode + "]")
 		}
+
+		Predicate<SiteNode> filterPredicate = { node -> return !predicates.stream().filter({ predicate -> return predicate.test(node) == false}).findFirst().isPresent()}
+		filterTree(msg, filterPredicate)
+
+	}
+	else {
+		println("Please select a valid page to filter!")
 	}
 	println("Done")
 }
@@ -119,19 +125,19 @@ boolean removeDefaultErrorPage(SiteMap sitesTree, SiteNode node, Predicate<SiteN
 			i--
 		}
 	}
-	if (predicate.test(node)) {
+	if (node.getChildCount() == 0 && predicate.test(node)) {
 		println("Removing Node: " + node.getHierarchicNodeName())
-		ExtensionHistory.purge(sitesTree, current)
-		return false
+		PopupMenuPurgeSites.purge(sitesTree, node)
+		return true
 	}
 	return false
 }
 
 void filterTree(HttpMessage msg, Predicate<SiteNode> predicate) {
 	SiteMap sitestree = Model.getSingleton().getSession().getSiteTree()
-	SiteNode node = sitestree.findNode(msg, true)
-	if (node != null) {
-		removeDefaultErrorPage(sitestree, node, predicate)
+	SiteNode rootNode = getRootNode(msg)
+	if (rootNode != null) {
+		removeDefaultErrorPage(sitestree, rootNode, predicate)
 	}
 }
 
@@ -143,24 +149,6 @@ HttpMessage request(HttpMessage origin, String path, boolean followRedirect) {
 	println(reqURI.toString())
 	sender.sendAndReceive(clone, followRedirect)
 	return clone
-}
-
-HttpMessage getErrorPage(HttpMessage msg) {
-	URI original = msg.getRequestHeader().getURI()
-	return request(msg, String.format("%s%s/%s", StringUtils.endsWith(original.getPath(), "/") ? original.getPath() : original.getPath() + "/", UUID.randomUUID().toString(),UUID.randomUUID().toString()), false)
-}
-
-HttpMessage getStartPage(HttpMessage msg) {
-	URI original = msg.getRequestHeader().getURI()
-	return request(msg, "/", true)
-}
-
-String getTitle(HttpMessage msg){
-	if (msg == null) return null
-	Source source = new Source(msg.getResponseBody().toString())
-	Element titleElement=source.getFirstElement(HTMLElementName.TITLE)
-	if (titleElement==null) return null
-	return CharacterReference.decodeCollapseWhiteSpace(titleElement.getContent())
 }
 
 String getBodyFromNode(SiteNode node) {
@@ -202,4 +190,23 @@ String getEndHashFromBody(SiteNode node, int lineCount) {
 		}
 	}
 	return null
+}
+
+boolean isValidStartPoint(HttpMessage msg) {
+	int statusCode = msg.getResponseHeader().getStatusCode()
+	return  statusCode != 0 && (!msg.getRequestHeader().isEmpty() || statusCode == 301 || statusCode == 302)
+}
+
+SiteNode getRootNode(HttpMessage msg) {
+	SiteMap sitestree = Model.getSingleton().getSession().getSiteTree()
+	SiteNode currentNode = sitestree.findNode(msg, false)
+	println(currentNode)
+	return getRootSiteNode(currentNode)
+}
+
+SiteNode getRootSiteNode(SiteNode node){
+	if (node.getParent() != null && node.getParent().getParent() != null) {
+		return getRootSiteNode(node.getParent())
+	}
+	return node
 }
