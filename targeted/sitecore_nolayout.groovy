112014d7dc3ac0511a50b644194e6c02d18f2c61
Tobias Gruber <gruber.tobias97@gmx.at>
2020-07-01 15:41:24 +0200

    adjusted sitecore groovy script

--- targeted/sitecore_nolayout.groovy
+++ targeted/sitecore_nolayout.groovy
@@ -15,42 +15,61 @@ import org.parosproxy.paros.extension.history.ExtensionHistory
 
 void invokeWith(HttpMessage msg) {
 	sender = new HttpSender(Model.getSingleton().getOptionsParam().getConnectionParam(), true, 6)
-	// Copy requests before reusing them
 	URI origin = msg.getRequestHeader().getURI()
 	msg = msg.cloneRequest()
-	// Inject your payload
 	msg.getRequestHeader().setMethod("GET")
+
+	// NO LAYOUT CHECK
 	reqUri = new URI(origin.getScheme(), origin.getAuthority(), "/sitecore/service/nolayout.aspx", null, null)
 	msg.getRequestHeader().setURI(reqUri)
 	sender.sendAndReceive(msg,false)
-	// Test the response here, and make other requests as required
-	validate(msg).ifPresent { evidence ->	extAlert = Control.getSingleton().getExtensionLoader().getExtension(ExtensionAlert.NAME)
-									href = addToHistory(msg)
-									alert = new Alert(50000, Alert.RISK_MEDIUM,Alert.CONFIDENCE_HIGH, 'Information Discloseure - Sensitive Application Information')
-									alert.setDescription('The product exposes sensitive information to an actor that is not explicitly authorized to have access to that information.')
-									alert.setOtherInfo('The repsonse appears to contain information which may help an attacker.')
-									alert.setSolution('Follow Sitecore Security Hardening Guide, see references')
-									alert.setEvidence(evidence)
-									alert.setCweId(200)
-									alert.setWascId(13)
-									alert.setReference('https://doc.sitecore.com/SdnArchive/upload/sitecore7/75/sitecore_security_hardening_guide-sc75-usletter.pdf')
-									alert.setHistoryRef(href)
-									alert.setMessage(msg)
-									alert.setUri(msg.getRequestHeader().getURI().toString())
-									extAlert.alertFound(alert,href)
-						}
+	checkResponseByCSSQuery(msg, "title:contains(Layout Not Found)").ifPresent{evidence -> raiseAlert(50000,
+															Alert.RISK_MEDIUM,
+															Alert.CONFIDENCE_HIGH,
+															"Information Discloseure - Sensitive Application Information",
+															"The product exposes sensitive information to an actor that is not explicitly authorized to have access to that information.",
+															"The repsonse appears to contain information which may help an attacker.",
+															"Follow Sitecore Security Hardening Guide, see references",
+															evidence,
+															200,
+															13,
+															"https://doc.sitecore.com/SdnArchive/upload/sitecore7/75/sitecore_security_hardening_guide-sc75-usletter.pdf",
+															msg) }
 }
 
-Optional<String> validate(HttpMessage msg) {
-	// First check if the status code is valid
-	if (msg.getResponseHeader().isText()) {
+
+
+
+
+Optional<String> checkResponseByCSSQuery(final HttpMessage msg, final String cssQuery) {
+	if (msg.getResponseHeader().isHtml()) {
 		return Optional.ofNullable(msg.getResponseBody().toString())
-								.map{ body -> Jsoup.parse(body, msg.getRequestHeader().getURI().toString()).select('title:contains(Layout Not Found)').first() }
+								.map{ body -> Jsoup.parse(body, msg.getRequestHeader().getURI().toString()).select(cssQuery).first() }
 								.filter{element -> element != null }
 								.map{ element -> element.outerHtml() }
 	}
 	return Optional.empty()
 }
+
+void raiseAlert(int pluginId, int risk, int confidence, String alertName, String description, String otherInfo,
+			 String solution, String evidence, int cweId, int wascId, String reference, HttpMessage msg) 
+{
+	extAlert = Control.getSingleton().getExtensionLoader().getExtension(ExtensionAlert.NAME)
+	href = addToHistory(msg)
+	alert = new Alert(pluginId, risk, confidence, alertName)
+	alert.setDescription(description)
+	alert.setOtherInfo(otherInfo)
+	alert.setSolution(solution)
+	alert.setEvidence(evidence)
+	alert.setCweId(cweId)
+	alert.setWascId(wascId)
+	alert.setReference(reference)
+	alert.setHistoryRef(href)
+	alert.setMessage(msg)
+	alert.setUri(msg.getRequestHeader().getURI().toString())
+	extAlert.alertFound(alert,href)
+}
+
 
 HistoryReference addToHistory(msg) {
 	extHistory = Control.getSingleton().getExtensionLoader().getExtension(ExtensionHistory.NAME)
